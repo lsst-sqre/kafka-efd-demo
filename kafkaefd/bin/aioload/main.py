8d8e5f9155fdb6ad4e8953a98b97b0f6e16ce0fd
Jonathan Sick <jsick@lsst.org>
2018-11-14 15:34:57 -0700

    Demonstrate aioload upload-schemas
    - The schemas for the value contains a timestamp-millis field that
      semantically encodes a unix timestamp with millisecond precision. This
      code demonstrates that datetime.datetime objects are successfully
      encoded and decoded.
    
    - The schemas are loaded from the schemas/ directory using the
      create_indexed_schema function. This function is useful because it
      lets us create duplicate schemas for each topic in the load test.

--- kafkaefd/bin/aioload/main.py
+++ kafkaefd/bin/aioload/main.py
@@ -5,7 +5,15 @@ frequency).
 
 __all__ = ('aioload',)
 
+import datetime
+from io import BytesIO
+import json
+from pathlib import Path
+
 import click
+import fastavro
+
+from ...salschema.convert import validate_schema
 
 
 @click.group()
@@ -17,3 +25,63 @@ def aioload(ctx):
     Messages are consumed and produced asynchronously with aiokafka.
     """
     ctx.obj = {}
+
+
+@aioload.command('test-schemas')
+@click.pass_context
+def test_schemas(ctx):
+    """Test Avro schemas.
+    """
+    value_schema = fastavro.parse_schema(
+        create_indexed_schema('simple-value', index=0))
+
+    print('value schema:')
+    print(json.dumps(value_schema, indent=2, sort_keys=True))
+
+    value_binary = BytesIO()
+    fastavro.schemaless_writer(
+        value_binary,
+        value_schema,
+        {'timestamp': datetime.datetime.now()}
+    )
+
+    # read it back
+    value_binary.seek(0)
+    value_data = fastavro.schemaless_reader(
+        value_binary, value_schema
+    )
+    print('Got value:')
+    print(value_data)
+
+
+def create_indexed_schema(name, index=0):
+    """Create a schema with an indexed namespace based on a template.
+
+    This is useful for generating many instances of a schema that can be used
+    by multiple topics in testing.
+
+    Parameters
+    ----------
+    name : `str`
+        Name, without ``.json`` suffix, of a schema file in the
+        ``aioload/schemas`` directory.
+    index : `int`
+        Index to rename the output schema. For example, if the namespace is
+        ``org.lsst.kafka_efd_demo.aioload.simple``, the new schema will have
+        a namespace of ``org.lsst.kafka_efd_demo.aioload.simple0`` given an
+        index of 0.
+
+    Returns
+    -------
+    schema : `dict`
+        Schema object with an indexed name.
+    """
+    source_path = Path(__file__).parent / 'schemas' / f"{name}.json"
+    with source_path.open() as f:
+        schema = json.load(f)
+    schema['namespace'] += f'{index:d}'
+
+    # validate the schema
+    validate_schema(schema, raise_error=True)
+
+    return schema
