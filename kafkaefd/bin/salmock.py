dde5b71c71466367c836801b89aae1c279bfa9e1
Jonathan Sick <jsick@lsst.org>
2018-12-04 12:10:05 -0700

    Add generator for mock avro-schema messages
    generate_message() analyzes the avro schema and creates generator
    functions for each field. Then it enters an infinite loop and yields
    random instances of the message.

--- kafkaefd/bin/salmock.py
+++ kafkaefd/bin/salmock.py
@@ -4,6 +4,8 @@
 __all__ = ('salmock',)
 
 import logging
+import random
+import datetime
 
 import click
 import structlog
@@ -33,6 +35,118 @@ def produce(ctx, log_level, prometheus_port):
     SAL topics with registered schemas.
     """
     configure_logging(level=log_level)
+
+
+def generate_message(schema):
+    """Generate mock messages for an Avro schema.
+
+    Parameters
+    ----------
+    schema : `dict`
+        Avro schema as a deserialized `dict`.
+
+    Yields
+    ------
+    message : `dict`
+        A message with random values appropriate to each fields's type.
+    """
+    logger = structlog.get_logger(__name__).bind(
+        schema=schema['name']
+    )
+
+    # Make random value generators for each field, according to the field's
+    # type.
+    field_generators = {}
+    for field in schema['fields']:
+        logger.debug('Making field generator', field=field['name'])
+        name = field['name']
+        if isinstance(field['type'], str):
+            # Basic field types
+            if field['type'] in ('int', 'long'):
+                field_generators[name] = generate_int
+            elif field['type'] == 'float':
+                field_generators[name] = generate_float
+            elif field['type'] == 'bytes':
+                field_generators[name] = generate_bytes
+            elif field['type'] == 'string':
+                field_generators[name] = generate_str
+            elif field['type'] == 'boolean':
+                field_generators[name] = generate_bool
+            else:
+                logger.error('Don\'t have generator for field type',
+                             field=name)
+                raise RuntimeError
+
+        elif isinstance(field['type'], dict):
+            # Complex field types
+            if 'logicalType' in field['type']:
+                if field['type']['logicalType'].startswith('timestamp'):
+                    field_generators[name] = generate_timestamp
+
+            elif 'type' in field['type']:
+                if field['type']['type'] == 'enum':
+                    field_generators[name] = EnumGenerator(
+                        field['type']['symbols'])
+
+            else:
+                logger.error('Don\'t have generator for field type',
+                             field=field)
+                raise RuntimeError
+
+        else:
+            logger.error('Don\'t have generator for field type',
+                         field=field)
+            raise RuntimeError
+
+    # Generate random messages infinitely
+    while True:
+        message = {n: gen() for n, gen in field_generators.items()}
+        logger.debug(message=message)
+        yield message
+
+
+def generate_bool():
+    """Generate a boolean, randomly."""
+    return random.choice((True, False))
+
+
+def generate_int():
+    """Generate an integer, randomly."""
+    return random.randint(0, 10)
+
+
+def generate_float():
+    """Generate a float, randomly."""
+    return random.random()
+
+
+def generate_bytes():
+    """Generate bytes, randomly."""
+    # Not sure what do generate for bytes. This is just zero-filled
+    return bytes(2)
+
+
+def generate_str():
+    """Generate a string, randomly."""
+    return random.choice(
+        ('James', 'Mary', 'John', 'Patricia', 'Robert', 'Jennifer',
+         'Michael', 'Linda', 'William', 'Elizabeth')
+    )
+
+
+def generate_timestamp():
+    """Generate a timestamp that matches the current time."""
+    return datetime.datetime.now(datetime.timezone.utc)
+
+
+class EnumGenerator:
+    """Callable that creates values from a enumeration."""
+
+    def __init__(self, symbols):
+        self.symbols = symbols
+
+    def __call__(self):
+        return random.choice(self.symbols)
 
 
 def configure_logging(level='info'):
