65aea888681e5d4091a219fe9d9a9ec2ef6d39bf
Angelo Fausti <afausti@lsst.org>
2019-05-06 17:53:29 -0700

    Add Oracle JDBC driver to kafka-connect image

--- k8s-cluster/cp-kafka-connect/build-docker.sh
+++ k8s-cluster/cp-kafka-connect/build-docker.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
-CONNECTOR="influxdb"
-CONNECTOR_VERSION="1.2.1"
-VERSION="2.1.0"
 IMAGE="lsstsqre/cp-kafka-connect"
 IMAGE_TAG="latest"
 
-echo "Building CP Kafka Connect with InfluxDB Sink Connector from Landoop"
-docker build \
-    --build-arg CONNECTOR=${CONNECTOR} \
-    --build-arg CONNECTOR_VERSION=${CONNECTOR_VERSION} \
-    --build-arg VERSION=${VERSION} \
-    -t ${IMAGE}:${IMAGE_TAG} \
-    -f Dockerfile .
+echo "Building Kafka Connect image"
+docker build -t ${IMAGE}:${IMAGE_TAG} -f Dockerfile .
 docker push ${IMAGE}:${IMAGE_TAG}
