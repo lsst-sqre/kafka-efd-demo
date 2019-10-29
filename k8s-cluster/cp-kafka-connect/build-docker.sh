262e1fed14b6a358d81180edcd9542b1532d4d57
Angelo Fausti <afausti@lsst.org>
2019-10-29 14:33:02 -0700

    Add replicator source connector plugin

--- k8s-cluster/cp-kafka-connect/build-docker.sh
+++ k8s-cluster/cp-kafka-connect/build-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 IMAGE="lsstsqre/cp-kafka-connect"
-IMAGE_TAG="latest"
+IMAGE_TAG="5.3.1"
 
 echo "Building Kafka Connect image"
 docker build -t ${IMAGE}:${IMAGE_TAG} -f Dockerfile .
